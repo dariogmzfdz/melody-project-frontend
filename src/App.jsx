c1549e874f30019f5d855120dca202139cec94b3
JOAN GAU GONELL <joangau18@hotmail.com>
2022-10-11 17:04:12 +0200

    edit user

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import SignInSide from '../src/Components/Login/Login.jsx';
+import Logout from '../src/Components/Logout.jsx';
+import { AuthProvider } from './hooks/useFirebase';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ForgotPassword from '../src/Components/Login/ForgotPassword.jsx';
+import FormPassword from '../src/Components/Login/formPassword.jsx';
+import Signup from '../src/Screens/SignIn/SignIn.jsx'
+import EditUser from './Components/EditUser/EditUser';
+
+function App() {
+
+  return (
+    <EditUser />
+    // <>
+//     // {/* <Router>
+// <AuthProvider> 
+  
+//   <Routes>
+//     <Route path='/' element={ <SignInSide/>} />
+//   <Route path="/forgot" element={<ForgotPassword/>} />
+//   <Route path="/signup" element={<Signup/>} />
+//   <Route path='/logout' element={ <Logout/> }/>
+//   <Route path='/form' element={ <FormPassword />}/>
+//   </Routes>
+//   </AuthProvider>
+//   </Router>
+//   </> */}
+//   // );
+)}
+
+export default App;
---
+++ src/components/EditUser/EditUser.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React from "react";
+import {  useState } from "react";
+// import { toast } from "react-toastify";
+
+const EditUser = () => {
+  const [userData, setUserData] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // const { state, dispatch: ctxDispatch } = useContext(state);
+  // const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const userId = await fetch("https://melody-music-stream-ten.vercel.app/login",
+    {
+    headers: {
+        auth_id: userId,
+    },}
+)
+
+    try {
+      const data = await axios.put(`https://melody-music-stream-ten.vercel.app/user/${userId}`,
+      {
+        name: name,
+        lastName: lastName,
+      })
+    console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(userData);
+  return (
+    <div>
+      <a href="#">
+        <h1> MELODY </h1>
+      </a>
+      <div className="formContainer">
+      <form onSubmit={submitHandler}>
+        <h3>Edit User</h3>
+        
+        <label>Name</label>
+        <input
+          className="signinInput"
+          placeholder="ej: John"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          required
+        ></input>
+        <label>Last Name</label>
+        <input
+          className="signinInput"
+          placeholder="ej: Smith"
+          id="lastName"
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        ></input>
+        
+        <button className="registerButton" type="submit">
+          Register
+        </button>
+      </form>
+      </div>
+      </div>    
+  );
+};
+
+export default EditUser;
