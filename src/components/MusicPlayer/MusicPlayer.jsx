bbb00443eb0882be192e8a3294d86fd5edc72946
JOAN GAU GONELL <joangau18@hotmail.com>
2022-10-24 16:10:04 +0200

    music player controls

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,11 +1,17 @@
-import React, { useRef } from "react";
-import {BsFillSkipStartCircleFill, BsFillPlayCircleFill, BsFillSkipEndCircleFill, BsFillPauseCircleFill} from 'react-icons/bs'
+import React, { useRef, useEffect, useState } from "react";
+import {BsFillSkipStartCircleFill, BsFillPlayCircleFill, BsFillSkipEndCircleFill, BsFillPauseCircleFill, BsFillHeartFill, BsHeart} from 'react-icons/bs'
 import './musicPlayer.scss';
 
-const MusicPlayer = ({audioElem, isPlaying, setIsPlaying, currentSong}) => {
+const MusicPlayer = ({audioElem, isPlaying, setIsPlaying, songs, currentSong, setCurrentSong}) => {
     const clickRef = useRef();
     const PlayPause = () => {
         setIsPlaying(!isPlaying);
+    }
+    
+    const [isFavourite, setIsFavourite ] = useState(false);
+    
+    const favourite = () => {
+        setIsFavourite(!isFavourite);
     }
     const checkWidth = (e)=> {
         let width = clickRef.current.clientWidth;
@@ -14,24 +20,45 @@ const MusicPlayer = ({audioElem, isPlaying, setIsPlaying, currentSong}) => {
         audioElem.current.currentTime = divprogress/100*currentSong.length;
     }
 
+    const skipBack = () => 
+    {
+      const index = songs.findIndex(x=>x.title== currentSong)
+      if (index = 0){
+        setCurrentSong(songs[songs.length-1])
+      }
+      else
+      {
+        currentSong = songs[index - 1]
+      }
+    }
+    // let minutes = Math.floor(audioElem.current.currentTime / 60);
+    // let seconds = (audioElem.current.currentTime % 60).toFixed();
+
+
   return (
     <div className="player_container">
       <div className="title">
         <p>{currentSong.title}</p>
+        {/* <p> {minutes} :  {seconds}</p> */}
       </div>
       <div className="navigation">
         <div className="navigation_wrapper" onClick={checkWidth} ref={clickRef}>
           <div className="seek_bar" style={{width: `${currentSong.progress+"%"}`}}></div>
         </div>
       </div>
+      <div className="controlButtons">
       <div className="controls">
-        <BsFillSkipStartCircleFill className="btn_action" />
+        <BsFillSkipStartCircleFill className="btn_action" onClick={skipBack} />
         {isPlaying ? <BsFillPauseCircleFill className="btn_action pp" onClick={PlayPause}/>
         :        <BsFillPlayCircleFill className="btn_action pp" onClick={PlayPause} />
     
     }
 
         <BsFillSkipEndCircleFill className="btn_action" />
+      </div>
+      <div className="favouriteSong">
+        {isFavourite ? <BsFillHeartFill className="heart_btn_action" onClick={favourite} /> : <BsHeart className="heart_btn_action" onClick={favourite} />}
+      </div>
       </div>
     </div>
   );
--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -1,38 +1,47 @@
-import React, { useEffect, useRef } from 'react'
-import { songsdata  } from './audios'
-import { useState } from 'react';
-import MusicPlayer from './MusicPlayer'
+import React, { useEffect, useRef } from "react";
+import { songsdata } from "./audios";
+import { useState } from "react";
+import MusicPlayer from "./MusicPlayer";
 
 const Player = () => {
-    const [songs, setSongs] = useState(songsdata);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentSong, setCurrentSong] = useState( songsdata[0] );
+  const [songs, setSongs] = useState(songsdata);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[0]);
 
-    const audioElem = useRef();
-    useEffect(()=>{
-        if(isPlaying){
-            audioElem.current.play();
-        }
-        else{
-            audioElem.current.pause()
-        }
-        
-    }, [isPlaying])
-
-    const onPlaying = () =>
-    {
-        const duration = audioElem.current.duration;
-        const ct = audioElem.current.currentTime;
-
-        setCurrentSong ({...currentSong, "progress": ct / duration * 100, "length": duration})
+  const audioElem = useRef();
+  useEffect(() => {
+    if (isPlaying) {
+      audioElem.current.play();
+    } else {
+      audioElem.current.pause();
     }
+  }, [isPlaying]);
 
-    return (
+  const onPlaying = () => {
+    const duration = audioElem.current.duration;
+    const ct = audioElem.current.currentTime;
+
+    setCurrentSong({
+      ...currentSong,
+      progress: (ct / duration) * 100,
+      length: duration,
+    });
+  };
+
+  return (
     <div>
-        <audio src={currentSong.url} ref={audioElem} onTimeUpdate={onPlaying} />
-        <MusicPlayer songs={songs} setSongs={setSongs} isPlaying={isPlaying} setIsPlaying={setIsPlaying} audioElem={audioElem} currentSong={currentSong}/>
+      <audio src={currentSong.url} ref={audioElem} onTimeUpdate={onPlaying} />
+      <MusicPlayer
+        songs={songs}
+        setSongs={setSongs}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        audioElem={audioElem}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
