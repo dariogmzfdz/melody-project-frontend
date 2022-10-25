be4bb080b552465fcfb5e031c3e4d4e5cc4dd13b
Agustín García <agustin.tavoite@gmail.com>
2022-10-25 13:58:53 +0200

    link error

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -58,7 +58,7 @@ export default function SignInSide() {
     e.preventDefault();
     try {
       const result = await axios.post(
-        "https://https://melodystream.herokuapp.com/user/login",
+        "https://melodystream.herokuapp.com/user/login",
         {
           email: datos.email,
           password: datos.password,
